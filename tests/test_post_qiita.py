0cc0f05aa8c474ae2d4e62014c54cbbd9b925eac
bluesDD <dd11blues@gmail.com>
2020-08-25 03:41:50 +0900

    :rotating_light: テスト追加する`

--- tests/test_post_qiita.py
+++ tests/test_post_qiita.py
@@ -1,5 +1,15 @@
 from post_qiita import __version__
+import os
+from post_qiita.get_posts import get_posts, get_filenames
 
+directory = os.path.dirname(__file__)
+target_dir = os.getcwd()
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_can_get_directory():
+    assert get_posts(directory) == target_dir
+
+def test_get_files_current_directory():
+    assert (os.path.basename(__file__) in get_filenames(directory)) == True
