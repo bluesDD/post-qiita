a708322434c953bd3a4e8f95a564922a363bab29
bluesDD <dd11blues@gmail.com>
2020-08-31 03:12:06 +0900

    tmp

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -8,6 +8,10 @@ def throw_valueerror():
     print(pathlib.Path)
     raise ValueError("This is test")
 
+def open_file(file):
+    with open(file) as f:
+        return f.read()
+
 class TestCase:
     def test_increment(self):
         assert increment(1) == 2, "足し算が違ってる"
@@ -16,3 +20,11 @@ class TestCase:
         with pytest.raises(ValueError) as error_info:
             throw_valueerror()
         assert str(error_info.value) == "This is test"
+
+    def test_open_file(self, tmp_path):
+        d = tmp_path / "sub"
+        d.mkdir()
+        p = d / "heelo.txt"
+        p.write_text("hello")
+        assert p.read_text() == "hello"
+
