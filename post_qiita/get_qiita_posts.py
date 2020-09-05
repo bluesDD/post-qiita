6dd7b14d5a9fcbd3c9806955ac41b3d287163c04
bluesDD <dd11blues@gmail.com>
2020-09-05 23:38:18 +0900

    :tada: Qittaの投稿を取得する`

--- post_qiita/get_qiita_posts.py
+++ post_qiita/get_qiita_posts.py
@@ -1,9 +1,9 @@
-import requests
-from secrets import QIITA_ACCESS_TOKEN
+# import requests
 import json
+from get_secrets import QIITA_TOKEN
+import requests
 
-headers = {"Authorization": f"Bearer {QIITA_ACCESS_TOKEN}"}
-print(headers)
+headers = {"Authorization": f"Bearer {QIITA_TOKEN}"}
 a = requests.get("https://qiita.com/api/v2/authenticated_user/items", headers=headers)
 
 posts = json.loads(a.text)
--- post_qiita/secrets.py
+++
@@ -1,3 +0,0 @@
-import get_dotenv
-
-QIITA_TOKEN = get_dotenv.qiita_token
